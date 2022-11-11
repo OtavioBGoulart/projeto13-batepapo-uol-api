0a7ec501eacee482c1ddbac4bc9088a3c43e9795
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-11-11 15:35:30 -0300

    refatoração do schema de validação nome de usuário

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import express, { json } from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv"
-import Joi from "joi";
-//import pkg from "joi"
-//const { valid } = pkg;
+import joi from "joi";
+
+
+const userSchema = joi.object({
+    name: joi.string().required()
+})
+
 
 
 const app = express();
@@ -14,37 +18,34 @@ app.use(cors());
 app.use(express.json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db;
+
 
 try {
     await mongoClient.connect();
-    db = mongoClient.db("Batepapo_UOL");
-    const participants = db.collection("participants");
-    const messages = db.collection("messages");
+    console.log("MongoDB conectado");
+
 } catch (err) {
     console.log(err);
 }
 
+const db = mongoClient.db("Batepapo_UOL");
+const participants = db.collection("participants");
+const messages = db.collection("messages");
+
 
 app.post("/participants", async (req, res) => {
 
+    const validation = userSchema.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+
+    res.send(validation.error.message);
+    return;
+    }
+    
     try {
-
-        const { name } = req.body;
-        console.log(name);
-
-        const schema = Joi.object({
-            name: Joi.string().required()
-        })
-
-        const validation = await schema.validateAsync(req.body);
-        console.log(validation);
         res.send("deu bom")
     } catch (error) {
-
-        if (error.isJoi === true) {
-            res.status(422).send("name deve ser strings não vazio");
-        }
         console.log("deu ruim");
     }
 
